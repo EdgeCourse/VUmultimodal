86b683435be75339400ecb556c4296d4a9c7587b
EdgeCourse <106573186+EdgeCourse@users.noreply.github.com>
2022-06-24 18:08:24 -0500

    Add files via upload

---
+++ structtags.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type User struct {
+	Name          string    `json:"name"`
+	Password      string    `json:"password"`
+	PreferredFish []string  `json:"preferredFish"`
+	CreatedAt     time.Time `json:"createdAt"`
+}
+
+func main() {
+	u := &User{
+		Name:      "Sammy the Shark",
+		Password:  "fisharegreat",
+		CreatedAt: time.Now(),
+	}
+
+	out, err := json.MarshalIndent(u, "", "  ")
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(out))
+}
+
+/*
+ignore field - otherwise, will print full
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type User struct {
+	Name      string    `json:"name"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+func main() {
+	u := &User{
+		Name:      "Sammy the Shark",
+		Password:  "fisharegreat",
+		CreatedAt: time.Now(),
+	}
+
+	out, err := json.MarshalIndent(u, "", "  ")
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(out))
+}
+*/
+
+/*
+//omit if empty
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type User struct {
+	Name          string    `json:"name"`
+	Password      string    `json:"password"`
+	PreferredFish []string  `json:"preferredFish,omitempty"`
+	CreatedAt     time.Time `json:"createdAt"`
+}
+
+func main() {
+	u := &User{
+		Name:      "Sammy the Shark",
+		Password:  "fisharegreat",
+		CreatedAt: time.Now(),
+	}
+
+	out, err := json.MarshalIndent(u, "", "  ")
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(out))
+}
+
+*/
